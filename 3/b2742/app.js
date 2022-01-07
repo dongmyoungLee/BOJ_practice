7b6305c53a211f4f40534881cf3c70c9100ec9de
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-01-07 21:49:27 +0900

    22/01/07_2742

---
+++ 3/b2742/app.js
@@ -0,0 +1,22 @@
+const { assert } = require('console');
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin'
+: './input.txt';
+
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+input = input.split(' ').map((item) => +item);
+
+
+solution(input[0], input[1]);
+
+
+function solution(A, B) {
+    let result = ''
+    for(i = A; i >= 1; i--) {
+        result += i + '\n'
+    }
+    console.log(result)
+}
