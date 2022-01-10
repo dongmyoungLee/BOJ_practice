0c70d9989105e490d457635dec2fa254c3ba1e4e
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-01-10 20:50:42 +0900

    22/01/10

---
+++ 3/b11021/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+let count = input[0];
+let numbers = [];
+let num = 1
+
+for (let i = 1; i < input.length; i++) {
+    if (input[i] !== '') {
+        numbers.push(input[i].split(' '));
+    }
+}
+for (let i = 0; i < numbers.length; i++){
+    let num1 = Number(numbers[i][0]);
+    let num2 = Number(numbers[i][1]);
+    console.log(`Case #${num + i}: ${num1 + num2}`)
+}
+
+
