905a86125014c057a9f84ad3c3226a6aa0b5c5a7
pajang1515 <73331335+dongmyoungLee@users.noreply.github.com>
2022-01-25 22:19:37 +0900

    te

---
+++ 5/b4344/app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let testCase = Number(input[0])
+let test = 0
